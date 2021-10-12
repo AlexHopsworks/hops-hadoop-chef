7899b0dd8a82b1df08e86a5fb0a5ee8345eb3512
Alex Ormenisan <aaor@kth.se>
2021-10-12 13:52:00 +0200

    hdfs certs

--- recipes/nn.rb
+++ recipes/nn.rb
@@ -1,23 +1,8 @@
 include_recipe "hops::default"
-
-template_ssl_server()
+include_recipe "hops::hdfs_certs"
 
 my_ip = my_private_ip()
 
-group node['hops']['secure_group'] do
-  action :modify
-  members node['hops']['hdfs']['user']
-  append true
-  not_if { node['install']['external_users'].casecmp("true") == 0 }
-end
-
-crypto_dir = x509_helper.get_crypto_dir(node['hops']['hdfs']['user'])
-kagent_hopsify "Generate x.509" do
-  user node['hops']['hdfs']['user']
-  crypto_directory crypto_dir
-  action :generate_x509
-  not_if { node["kagent"]["enabled"] == "false" }
-end
 
 file "#{node['hops']['conf_dir']}/dfs.exclude" do 
   owner node['hops']['hdfs']['user']
